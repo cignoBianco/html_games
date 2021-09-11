ad2e543942fa3ce59ebc5e2a981b77f62e7008af
cignobianco <smiana123@gmail.com>
2021-09-12 00:01:04 +0700

    chapter 1: image loader

--- canvas0/javascript/dynamicImage.js
+++ canvas0/javascript/dynamicImage.js
@@ -3,5 +3,5 @@ let image = new Image();
 image.src = "images/shipsprites.png";
 // document.querySelector("body").appendChild(image);
 image.onload = function() {
-    alert("Image finished loading");
+    // alert("Image finished loading");
 }
